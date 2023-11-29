ebb59ef39d9ac2048667f0fd3a22a213276e56be
Larry Latorre <llatorre481@gmail.com>
2023-11-28 21:31:45 -0600

    display col

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,20 +1,26 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 
 import HornedBeast from './HornedBeast';
-
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 const Gallery = ({ beastData }) => {
   const beastComponents = beastData.map((beast) => (
+    <Col key={beast._id} xs={12} sm={6} md={4} lg={3}>
     <HornedBeast
       key={beast._id}
       title={beast.title}
       imageUrl={beast.image_url}
       description={beast.description}
     />
+    </Col>
   ));
 
-  return <div className="gallery">{beastComponents}</div>;
+  return( <Container fluid>
+    <Row>{beastComponents}</Row>
+  </Container>
+  );
 };
 
 export default Gallery;
--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
-import Gallery from "./Gallery"
 import { useState } from "react";
 import { Container, Row , Col, Image} from "react-bootstrap";
 const HornedBeast = ({ title, imageUrl, description }) => {
@@ -13,22 +12,12 @@ function handleClick(){
     }
 }
     return(
-        <>
-        
+    <>
         <h2>Gallery of Horns</h2>
-        <Container>
-            <Row>
-                <Col>
-                <Image src={imageUrl} alt={title} title={title} rounded fluid></Image>
-                  <h3 onClick={handleClick}>{status}</h3> 
-                  
-                  
-                <p>{description}</p>
-                </Col>
-            </Row>
-         </Container>
-       
-        </>
+        <Image src={imageUrl} alt={title} title={title} rounded fluid></Image>
+        <h3 onClick={handleClick}>{status}</h3> 
+        <p>{description}</p>
+    </>
         
     )
 }
